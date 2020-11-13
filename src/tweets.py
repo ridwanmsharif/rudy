67188cbd47c4b2c2408b2cf2f37eabc4bdd5d38b
a-sohail <abdullah.sohail@hotmail.com>
2020-11-12 19:56:57 -0500

    Tested out tweets class

--- src/tweets.py
+++ src/tweets.py
@@ -11,30 +11,21 @@ class Tweets:
 
     def get_all_tweets(self, screen_name):
 
-        #initialize a list to hold all the tweepy Tweets
         tweets = []
+        oldest = None
 
-        #make initial request for most recent tweets (200 is the maximum allowed count)
-        new_tweets = self.api.user_timeline(screen_name=screen_name, count=200)
+        while True:
+            if oldest is not None:
+                new_tweets = self.api.user_timeline(screen_name=screen_name, count=200, max_id=oldest)
+            else:
+                new_tweets = self.api.user_timeline(screen_name=screen_name, count=200)
 
-        #save most recent tweets
-        tweets.extend(new_tweets)
+            if len(new_tweets) == 0:
+                break
 
-        #save the id of the oldest tweet less one
-        oldest = tweets[-1].id - 1
-
-        #keep grabbing tweets until there are no tweets left to grab
-        while len(new_tweets) > 0:
-
-            #all subsequent requests use the max_id param to prevent duplicates
-            new_tweets = self.api.user_timeline(screen_name=screen_name, count=200, max_id=oldest)
-
-            #save most recent tweets
             tweets.extend(new_tweets)
-
-            #update the id of the oldest tweet less one
             oldest = tweets[-1].id - 1
-
             print(f"{len(tweets)} tweets downloaded so far")
 
         return tweets
+
